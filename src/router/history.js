7560afe8fdb24e9bd7013c255e2e2ddecf80a58f
NicoBurno <nicoburno@gmail.com>
2019-01-15 23:48:17 +0300

    Init project

---
+++ src/router/history.js
@@ -0,0 +1,8 @@
+// @flow
+
+// $FlowFixMe react-router-dom + history (incompatible)
+import createBrowserHistory from 'history/createBrowserHistory';
+
+import type { RouterHistory } from 'react-router-dom';
+
+export default ((): RouterHistory => createBrowserHistory())();
